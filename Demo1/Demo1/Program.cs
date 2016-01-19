35196ae10e94b7bf0aa7e9590b8de0ea08b4f7f0
K2961 <aksim91@gmail.com>
2016-01-19 18:05:44 +0200

    Tehtävä 1 tehty

--- Demo1/Demo1/Program.cs
+++ Demo1/Demo1/Program.cs
@@ -10,6 +10,30 @@ namespace Demo1
     {
         static void Main(string[] args)
         {
+            Harjoitus1();
+
+            Console.ReadLine();
+        }
+        static void Harjoitus1()
+        {
+            Console.Write("Anna luku 1, 2 tai 3 ");
+            int luku = int.Parse(Console.ReadLine());
+            switch (luku)
+            {
+                case 1:
+                    Console.Write("Annoit luvun: Yksi");
+                    break;
+                case 2:
+                    Console.Write("Annoit luvun: Kaksi");
+                    break;
+                case 3:
+                    Console.Write("Annoit luvun: Kolme");
+                    break;
+                default:
+                    Console.Write("joku muu luku");
+                    break;
+
+            }
         }
     }
 }
