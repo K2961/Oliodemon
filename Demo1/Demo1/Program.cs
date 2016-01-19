69afb546862436c97b42b0eccd3c0fa6e258447b
K2961 <aksim91@gmail.com>
2016-01-19 19:27:22 +0200

    Harjoitus 6 tehty !

--- Demo1/Demo1/Program.cs
+++ Demo1/Demo1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Demo1
 {
@@ -10,13 +11,17 @@ namespace Demo1
     {
         static void Main(string[] args)
         {
-            Harjoitus5();
+            Harjoitus1();
 
             Console.ReadLine();
         }
-        //Tehtävä 1 alkaa!
+        /// <summary>
+        /// Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana (yksi, kaksi tai kolme). 
+        /// Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku". 
+        /// </summary>
         static void Harjoitus1()
         {
+            
             Console.Write("Anna luku 1, 2 tai 3 ");
 
             int luku = int.Parse(Console.ReadLine());
@@ -38,8 +43,9 @@ namespace Demo1
             }
         }
 
-        // Tehtävä 2 alkaa!
-
+        /// <summary>
+        /// Tee ohjelma, jossa annetaan oppilaalle koulunumero seuraavan taulukon mukaan (pistemäärä kysytään ja ohjelma tulostaa numeron): 
+        /// </summary>
         static void Harjoitus2()
         {
             int numero;
@@ -89,7 +95,9 @@ namespace Demo1
             }
         }
 
-        // Tehtävä 3 alkaa !
+        /// <summary>
+        /// Tee ohjelma, joka kysyy käyttäjältä kolme lukua ja tulostaa niiden summan ja keskiarvon. 
+        /// </summary>
 
         static void Harjoitus3()
         {
@@ -107,7 +115,10 @@ namespace Demo1
             Console.Write("Lukujen summa on:  " + ( luku1 + luku2 + luku3));
 
         }
-        // Harjoitus 4 alkaa
+        /// <summary>
+        /// Tee ohjelma, jossa kysytään käyttäjältä tämän ikä. Jos ikä on alle 18 vuotta, tulosta "alaikäinen", 
+        /// jos se on 18-65 vuotta, tulosta "aikuinen", muussa tapauksessa tulosta "seniori". 
+        /// </summary>
         static void Harjoitus4()
         {
             Console.Write("Anna ikäsi: ");
@@ -128,8 +139,9 @@ namespace Demo1
             }
         }
 
-        //Harjoitus 5 alkaa
-
+        /// <summary>
+        /// Tee ohjelma, joka näyttää annetun sekuntimäärän tunteina, minuutteina ja sekunteina. Aikamääre sekuntteina kysytään käyttäjältä.
+        /// </summary>
         static void Harjoitus5()
         {
             Console.Write("Anna sekunnit: ");
@@ -141,6 +153,24 @@ namespace Demo1
             int sek = sekunnit % 60;
 
             Console.Write("Antamasi sekunttiaika voidaan ilmaista muodossa: " + tun + "  Tuntia  " + min + "  Minuuttia  " + sek + "  Sekunttia." + "\nHienommin merkattuna:  " + tun + ":" + min + ":" + sek);
+        }
+
+        /// <summary>
+        /// Auton kulutus on 7.02 litraa 100 kilometrin matkalla ja bensan hinta on 1.595 Euroa. 
+        /// Tee ohjelma, joka tulostaa ajetulla matkalla (kysytään käyttäjältä) kuluvan bensan määrän sekä bensaan menevän rahan määrän. 
+        /// </summary>
+
+        static void Harjoitus6()
+        {
+            Console.Write("Anna matka:  ");
+            double matka = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); // Alustaa matkan ja lukee käyttäjän antaman tiedon sekä ottaa pilkun käyttöön desimaaleissa.
+
+            double litrat = matka / 100 * 7.02;
+            double kustannus = litrat * 1.595;
+
+
+            Console.Write("Bensaa kuluu " + Math.Round(litrat, 2) + " litraa, kustannus " + Math.Round(kustannus, 2) + " euroa.");
+
         }
     }
 }
