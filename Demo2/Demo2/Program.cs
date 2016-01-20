903c66b2cdaf12eb772021d3497e67e503de7baf
K2961 <aksim91@gmail.com>
2016-01-20 19:38:15 +0200

    Ehkä jotain muutosta

--- Demo2/Demo2/Program.cs
+++ Demo2/Demo2/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Demo2
 {
+    class Kiuas
+    {
+        public Kiuas(string Virtakytkin)
+        {
+            this.Virtakytkin = Virtakytkin;
+
+        }
+        public string OnVOff
+        {
+            get { return Virtakytkin; }
+        }
+        public string Virtakytkin;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Kiuas Suomi = new Kiuas();
+            Console.WriteLine("Kiuas on: " + Suomi.Virtakytkin);
+            Console.ReadLine();
         }
     }
 }
