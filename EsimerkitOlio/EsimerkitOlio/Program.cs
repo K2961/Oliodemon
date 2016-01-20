c88ce1bd44d444e7233a0d029a233aa516196be8
K2961 <aksim91@gmail.com>
2016-01-20 17:54:06 +0200

    Olio-ohjelmoinnin johdanto esimerkki

---
+++ EsimerkitOlio/EsimerkitOlio/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsimerkitOlio
+{
+    class Car
+    {
+        public Car(string model, int cc)
+        {
+            this.model = model;
+            this.cc = cc;
+        }
+
+        public int EngineSize
+        {
+            get { return cc; }
+        }
+        
+
+        public string ModelProperty
+        {
+            get { return model; }
+            set{ model = value; }
+        }
+        private string model;
+        private readonly int cc;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car toyota = new Car("avensis", 1895);
+            toyota.ModelProperty = "911";
+
+            Console.WriteLine("This toyota model is: " + toyota.ModelProperty);
+            Console.WriteLine("This toyota engine size is: " + toyota.EngineSize);
+            Console.ReadLine();
+        }
+    }
+}
