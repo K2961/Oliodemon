865e6cc4e0e1a1faef955f9815be4b1f2c2cbba4
K2961 <aksim91@gmail.com>
2016-01-20 18:54:59 +0200

    Demo2 projekti luotu

--- EsimerkitOlio/EsimerkitOlio/Program.cs
+++ EsimerkitOlio/EsimerkitOlio/Program.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace EsimerkitOlio
 {
-    class Car
-    {
+    //class Car
+    /*{
         public Car(string model, int cc)
         {
             this.model = model;
@@ -27,17 +27,36 @@ namespace EsimerkitOlio
         }
         private string model;
         private readonly int cc;
+    }*/
+    struct Point
+    {
+        public int x;
+        public int y;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Car toyota = new Car("avensis", 1895);
+            /*Car toyota = new Car("avensis", 1895);
             toyota.ModelProperty = "911";
 
             Console.WriteLine("This toyota model is: " + toyota.ModelProperty);
             Console.WriteLine("This toyota engine size is: " + toyota.EngineSize);
+            Console.ReadLine();*/
+
+            Point coordinate;
+            coordinate.x = 100;
+            coordinate.y = 50;
+
+            ModifyPoint(coordinate);
+
+            Console.WriteLine("Coordinate is: " + coordinate.x + "," + coordinate.y);
             Console.ReadLine();
+        }
+        static void ModifyPoint(Point p)
+        {
+            p.x += 500;
+            p.y += 1500;
         }
     }
 }
