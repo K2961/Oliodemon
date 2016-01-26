ae271a6299714d0afb88cbf12470ddcb73d4824e
K2961 <aksim91@gmail.com>
2016-01-26 18:30:42 +0200

    Perinnöllisyys Teacher, Student ja Person

--- Demo4/Demo4/Program.cs
+++ Demo4/Demo4/Program.cs
@@ -10,9 +10,40 @@ namespace Demo4
     {
         static void Main(string[] args)
         {
-            Person somebody = new Person("John", "Unknown");
+            Person somebody = new Person();
+            somebody.FirstName = "Joe";
+            somebody.LastName = "Dirt";
+            somebody.Address = "Unknown";
+            somebody.Age = 29;
+            somebody.PhoneNumber = "5465446548645465";
+            Console.WriteLine(somebody.ToString());
+
+            Teacher teacher = new Teacher();
+            teacher.FirstName = "Teppo";
+            teacher.LastName = "Konsoli";
+            teacher.Address = "piippukatu 2";
+            teacher.Age = 40;
+            teacher.PhoneNumber = "010-12345678";
+            teacher.Room = "D100";
+            Console.WriteLine(teacher.ToString());
+
+            Student student = new Student("Minna", "Husso", "J9090");
+            student.Address = "kilju 3";
+            student.Age = 20;
+            student.PhoneNumber = "040-12345678";
+            Console.WriteLine(student.ToString());
+
             somebody.PersonMethod();
-            Console.WriteLine();
+            teacher.PersonMethod();
+            teacher.TeacherMethod();
+            student.PersonMethod();
+            student.StudentMethod();
+
+            // nonono!
+            //teacher.StudentMethod();
+            //student.TeacherMethod();
+
+            Console.ReadLine();
         }
     }
 }
