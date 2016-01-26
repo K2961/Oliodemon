9fe6fb1b55affed59780dacb715eef0d301d0096
K2961 <aksim91@gmail.com>
2016-01-26 10:28:02 +0200

    Demo 3 lisätty

--- Demo1/Demo1/Program.cs
+++ Demo1/Demo1/Program.cs
@@ -332,5 +332,22 @@ namespace Demo1
             }
 
         }
+
+        /// <summary>
+        /// Tehtävä 12
+        /// Tee ohjelma, joka kysyy käyttäjältä 5 kokonaislukua.Luvut tulee sijoittaa taulukkoon.Ohjelman tulee tulostaa annetut luvut käänteisessä järjestyksessä.
+        /// Esimerkkitoiminta:
+        /// Anna Luku > 1 [Enter]
+        /// Anna Luku > 2 [Enter]
+        /// Luku > 3 [Enter]
+        /// Anna Luku > 4 [Enter]
+        /// Anna Luku > 5 [Enter]
+        /// Luvut ovat 5,4,3,2,1
+        /// </summary>
+
+        static void Harjoitus12()
+        {
+
+        }
     }
 }
---
+++ Demo3/Demo3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Harjoitus4();
+
+            Console.ReadLine();
+        }
+
+        static void Harjoitus4()
+        {
+
+        }
+    }
+}
