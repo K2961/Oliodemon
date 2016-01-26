2b8d2f1215f3137a50893f122162f1f7c1f37868
K2961 <aksim91@gmail.com>
2016-01-26 19:32:59 +0200

    Tehtävä 3 esimerkki valmis

---
+++ Demo4/Harjoitus3/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus3
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public float Salary
+        {
+            get { return salary; }
+            set { if (value >= 0.0f) salary = value; else salary = 0.0f;}
+        }
+
+        public Employee(string name, string profession, float salary)
+        {
+            Name = name;
+            Profession = profession;
+            Salary = salary;
+        }
+
+        private float salary;
+    }
+}
---
+++ Demo4/Harjoitus3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee kirsi = new Employee("Kirsi Kernel", "Teacher", 4200.50f);
+            Boss jussi = new Boss("Jussi Jurkka", "Head of institute", 9000.0f, "Audi", 5000.0f);
+
+            kirsi.Salary += 1000.0f;
+            jussi.Salary -= 2000.0f;
+            jussi.Bonus = 1000.0f;
+            //kirsi.Bonus = nocando
+        }
+    }
+}
