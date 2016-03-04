f701eebf585137f6395c080cdf14a802ddffbbd7
K2961 <aksim91@gmail.com>
2016-03-04 21:41:00 +0200

    Demo7 Task 2 Done. Demo 7 Task 3 added.

--- Demo7/Demo7/Program.cs
+++ Demo7/Demo7/Program.cs
@@ -1,47 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo7
+/// <summary>
+/// Tehtävä 2 - luvut tiedostoihinhome Kotitehtävä
+/// Tee ohjelma, joka kysyy käyttäjältä lukuja(joko kokonaisluku tai reaaliluku) ja tallenna kokonaisluvut eri tiedostoon kuin reaaliluvut.Sovellus tulee lopettaa, jos käyttäjä ei syötä kokonais- tai reaalilukua.Tarkista tiedostojen sisältö jollain tekstieditorilla.
+/// Esimerkkitulostus:
+/// Give a number (enter or not a number ends) : 1,0
+/// Give a number(enter or not a number ends) : 2,0
+/// Give a number(enter or not a number ends) : 3,0
+/// Give a number(enter or not a number ends) : 4
+/// Give a number(enter or not a number ends) : 5
+/// Give a number(enter or not a number ends) : 6
+/// Give a number(enter or not a number ends) :
+/// Contents of T2Integers.txt:
+/// 4
+/// 5
+/// 6
+/// Contents of T2Doubles.txt:
+/// 1,0
+/// 2,0
+/// 3,0
+/// /// </summary>
+/// 
+namespace Demo7Task2
 {
     class Program
     {
         static void Main(string[] args)
         {
- 
-        Console.WriteLine("Welcome to the ultimate NUMBERS saving program!");
-            SaveNumbers one = new SaveNumbers();
-            one.WorkingI();
 
-            do
+            string line;
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter integerFile = new StreamWriter(mydocpath + @"\TestIntegers.txt"))
             {
-                Console.WriteLine("\n::::::::::::::::Menu::::::::::::::::::");
-                Console.WriteLine("\nAnna numerot tallennettavaksi painamalla 1 ");
-                Console.WriteLine("\nTallenna numero tiedostoon painamalla 2 ");
-                Console.WriteLine("\nLopeta ohjelman käyttö painamalla 0 ");
-                int Choice = int.Parse(Console.ReadLine());
-
-                switch (Choice)
-                {  
-                    case 0:
-                        one.Working0();
-                        break;
-                    case 1:
-                        one.GetNumbers();
-                        break;
-                    case 2:
-                        System.IO.StreamWriter outputFile = new System.IO.StreamWriter("test.txt");
-                        outputFile.WriteLine(one.Line);
-                        outputFile.Close();
-                        break;
+                using (StreamWriter doubleFile = new StreamWriter(mydocpath + @"\TestDoubles.txt"))
+                {
+                    do
+                    {
+                        Console.Write("Give a number (enter or not a number ends) : ");
+                        line = Console.ReadLine();
+                        if (line.Length != 0)
+                        {
+                            try
+                            {
+                                int number = int.Parse(line);
+                                integerFile.WriteLine(line);
+                            }
+                            catch (FormatException)
+                            {
+                                try
+                                {
+                                    double number = double.Parse(line);
+                                    doubleFile.WriteLine(line);
+                                }
+                                catch (FormatException)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    } while (line.Length != 0);
                 }
+            }
+            
+            try
+            {
+                string text1 = File.ReadAllText(mydocpath + @"\TestIntegers.txt");
+                Console.WriteLine("Contents of TestIntegers.txt:\n" + text1);
+                string text2 = File.ReadAllText(mydocpath + @"\TestDoubles.txt");
+                Console.WriteLine("Contents of TestDoubles.txt:\n" + text2);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
 
-            } while (one.Working == true);
-
-            Console.WriteLine("Good Bye!");
-            Console.WriteLine("Press Enter to exit.");
+            Console.WriteLine();
+            Console.WriteLine("Press enter key to continue...");
             Console.ReadLine();
         }
     }
---
+++ Demo7/Demo7Task3/TVProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo7Task3
+{
+    class TVProgram
+    {
+        public string Name { get; set; }
+        public string Channel { get; set; }
+        public int StartingTime { get; set; }
+        public int EndingTime { get; set; }
+        public string Description { get; set; }
+
+       
+    }
+}
