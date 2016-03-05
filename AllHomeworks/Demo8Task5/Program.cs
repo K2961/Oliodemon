ce850f93e48c7ec00f31ff9e217bb672d4e09527
K2961 <aksim91@gmail.com>
2016-03-05 21:25:17 +0200

    Demo 8 Task 5 Done.

--- AllHomeworks/Demo8Task5/Program.cs
+++ AllHomeworks/Demo8Task5/Program.cs
@@ -10,6 +10,15 @@ namespace Demo8Task5
     {
         static void Main(string[] args)
         {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+
+            Console.WriteLine("Sum = {0:F2}", ArrayCalcs.Sum(array));
+            Console.WriteLine("Ave = {0:F2}", ArrayCalcs.Average(array));
+            Console.WriteLine("Min = {0:F2}", ArrayCalcs.Min(array));
+            Console.WriteLine("Max = {0:F2}", ArrayCalcs.Max(array));
+
+            Console.WriteLine("Press enter key to continue...");
+            Console.Read();
         }
     }
 }
