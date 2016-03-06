44de8a3a7a89e7ee3b208b0b9a724e61683b9833
K2961 <aksim91@gmail.com>
2016-03-07 00:04:24 +0200

    Demo 9 Task 3 Done.

--- Demo9/Demo9Task3/MainPage.xaml.cs
+++ Demo9/Demo9Task3/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,55 @@ namespace Demo9Task3
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // check
+            bool numbers = true;
+
+            // get values, should check that are numbers
+            double windowLength;
+            double windowHeight;
+            double kWidth;
+
+            if (!double.TryParse(lengthTextBox.Text, out windowLength))
+            {
+                numbers = false;
+            }
+            if (!double.TryParse(heightTextBox.Text, out windowHeight))
+            {
+                numbers = false;
+            }
+            if (!double.TryParse(kWidthTextBox.Text, out kWidth))
+            {
+                numbers = false;
+            }
+
+            if (!numbers)
+            {
+                var dialog = new MessageDialog("Check Values!");
+                await dialog.ShowAsync();
+            }
+            else {
+                // check values
+                if (windowLength < 2 * kWidth || windowHeight < 2 * kWidth)
+                {
+                    var dialog = new MessageDialog("Check Values (length or height can't smaller than 2*W)!");
+                    await dialog.ShowAsync();
+                }
+                else {
+                    // calc values
+                    double windowArea = windowLength * windowHeight / 10; // cm^2
+                    double classArea = (windowLength - 2 * kWidth) * (windowHeight - 2 * kWidth) / 10; // cm^2
+                    double kCircuit = (windowLength * 2 + windowHeight * 2) / 10; // cm
+
+                    // show values
+                    windowAreaTextBox.Text = windowArea.ToString() + " cm^2";
+                    classAreaTextBox.Text = classArea.ToString() + " cm^2";
+                    kCircuitTextBox.Text = kCircuit + " cm";
+                }
+            }
         }
     }
 }
