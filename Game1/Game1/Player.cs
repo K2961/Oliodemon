90f20317042c6472f5cfd6af279c5be3a76b0ff3
K2961 <aksim91@gmail.com>
2016-03-21 03:28:59 +0200

    Add GameTest

---
+++ Game1/Game1/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shooter
+{
+    class Player
+    {
+        public Texture2D PlayerTexture;
+        public Vector2 Position;
+        public bool Active;
+        public int Healt;
+
+        public int Width
+        {
+            get { return PlayerTexture.Width; }
+        }
+
+        public int Heigth
+        {
+            get { return PlayerTexture.Height; }
+        }
+
+        public void Initialize(Texture2D texture,Vector2 position)
+        {
+            //Player picture
+            PlayerTexture = texture;
+
+            Position = position;
+
+            Active = true;
+
+            Healt = 100;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
