a499430fe17cdb17824b358052ba5553250fb81a
K2961 <aksim91@gmail.com>
2016-04-13 13:23:11 +0300

    Add Test1 in Practice

---
+++ Practice/Test1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random number test
+            int randomnumber;
+
+            Random rand = new Random();
+
+            do
+            {
+                randomnumber = rand.Next(10);
+                Console.WriteLine("Next random number is: " + randomnumber);
+
+            } while (randomnumber != 9);
+
+            Console.WriteLine("The show is over." + "\nPress enter to exit..");
+            Console.ReadLine();
+        }
+    }
+}
